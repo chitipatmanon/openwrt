9e93e3f638ea263480c55f531a0ef01aaeeef4f4
Mr. Chitipat Manon <244321266+chitipatmanon@users.noreply.github.com>
2025-11-22 15:38:35 +0700

    Update diy-part2.sh

--- config/openwrt_main/diy-part2.sh
+++ config/openwrt_main/diy-part2.sh
@@ -42,7 +42,8 @@ git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 rm -rf package/openclash
 git clone https://github.com/vernesong/OpenClash.git package/openclash
 
-# Add core
+# Add clash-core
+mkdir -p files/etc/openclash/core
 wget -qO- https://github.com/vernesong/OpenClash/blob/core/master/smart/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash  
 wget -qO- https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/meta/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash_meta
 chmod +x files/etc/openclash/core/clash*
