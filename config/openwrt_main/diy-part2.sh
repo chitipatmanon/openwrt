8ab9b1c4d97539b8a12d2b0eb2f4fc9b81eeef41
Mr. Chitipat Manon <244321266+chitipatmanon@users.noreply.github.com>
2025-11-22 15:14:49 +0700

    Update diy-part2.sh

--- config/openwrt_main/diy-part2.sh
+++ config/openwrt_main/diy-part2.sh
@@ -18,7 +18,7 @@ ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01
 }
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$pSFNodTy$ej92Jju6QPD9AIAuelgnr.:18993:0:99999:7:::' package/base-files/files/etc/shadow
+sed -i "s/root::0:0:99999:7:::/root:"'$'"1"'$'"pSFNodTy"'$'"ej92Jju6QPD9AIAuelgnr.:18993:0:99999:7:::/g" package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
