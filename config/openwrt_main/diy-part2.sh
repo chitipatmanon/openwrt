d3ce486241c21a5bf6fe3a4f9c5eed915cc0e91b
tvguideone <tvguide.one@outlook.com>
2025-11-22 14:49:14 +0700

    Initial commit

---
+++ config/openwrt_main/diy-part2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: main
+#========================================================================================================================
+
+# ------------------------------- Main source started -------------------------------
+#
+# Set default IP address
+default_ip="192.168.1.1"
+ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+# Modify default IP if an argument is provided and it matches the IP format
+[[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
+    echo "Modify default IP address to: ${1}"
+    sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/files/bin/config_generate
+}
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$pSFNodTy$ej92Jju6QPD9AIAuelgnr.:18993:0:99999:7:::' package/base-files/files/etc/shadow
+
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEREPO='github.com/openwrt/openwrt'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEBRANCH='main'" >>package/base-files/files/etc/openwrt_release
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+# Add modemfeed
+rm -rf package/modemfeed
+git clone https://github.com/koshev-msk/modemfeed.git package/modemfeed
+
+# Add luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+# Add luci-app-openclash
+rm -rf package/openclash
+git clone https://github.com/vernesong/OpenClash.git package/openclash
+
+# Add core
+wget -qO- https://github.com/vernesong/OpenClash/blob/core/master/smart/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash  
+wget -qO- https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/meta/clash-linux-arm64.tar.gz | tar xOvz > files/etc/openclash/core/clash_meta
+chmod +x files/etc/openclash/core/clash*
+
+# Apply patch
+# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
+#
+# ------------------------------- Other ends -------------------------------
